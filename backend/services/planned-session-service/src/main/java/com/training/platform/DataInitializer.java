cb71c54b5f7efb7c5ea18ef8580c9580a4b9e4e5
ammar2050 <92795013+ammar2050@users.noreply.github.com>
2026-05-07 04:59:34 +0100

    Update DataInitializer.java

--- backend/services/planned-session-service/src/main/java/com/training/platform/DataInitializer.java
+++ backend/services/planned-session-service/src/main/java/com/training/platform/DataInitializer.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
-@Component
+//@Component
 public class DataInitializer implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
